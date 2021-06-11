5d6b82a529f43124cc71374ff38423890f56b602
Skulllady <noorhoque45@gmail.com>
2021-06-11 15:29:51 -0400

    added create account form component

--- react-app/src/components/accounts/CreateAccount.js
+++ react-app/src/components/accounts/CreateAccount.js
@@ -25,8 +25,51 @@ const CreateAccountForm = () => {
 
     await dispatch(createAccount(payload));
     dispatch(displayAccounts());
+
+    setAccountNumber("")
+    setAccountName("")
+    setAccountType("")
+    setInstitution("")
+    setBalance("")
   }
 
+  const updateAccountNumber = (e) => {
+    setAccountNumber(e.target.value);
+  }
 
+  const updateAccountName = (e) => {
+    setAccountName(e.target.value);
+  }
 
+  const updateAccountType = (e) => {
+    setAccountType(e.target.value);
+  }
+
+  const updateInstitution = (e) => {
+    setInstitution(e.target.value);
+  }
+
+  const updateBalance = (e) => {
+    setBalance(e.target.value);
+  }
+
+  return (
+    <form onSubmit={createAccountOnSubmit}>
+      <label>Account Number</label><input></input>
+      <label>Account Name</label><input></input>
+      <label>Account Type</label>
+      <div>
+        <select>
+          <option value="">--</option>
+          <option value="Checking Account">--</option>
+          <option value="Savings Account"></option>
+          <option value="Loan Account"></option>
+          <option value="Investment Account"></option>
+        </select>
+      </div>
+      <label>Institution</label><input></input>
+      <label>Current Balance</label><input></input>
+      <button></button>
+    </form>
+  )
 }
