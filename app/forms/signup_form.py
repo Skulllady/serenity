c573b077a497a7d5d6f97cbef6b091a3992dc691
Skulllady <noorhoque45@gmail.com>
2021-06-14 02:18:51 -0400

    sidebar styling

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -4,15 +4,21 @@ from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 
-def user_exists(form, field):
+def email_exists(form, field):
     print("Checking if user exits", field.data)
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
-        raise ValidationError("User is already registered.")
+        raise ValidationError("This e-mail is already registered.")
 
+def username_exists(form, field):
+    print("Checking if email exits", field.data)
+    username = field.data
+    user = User.query.filter(User.username == username).first()
+    if user:
+        raise ValidationError("This username is already registered.")
 
 class SignUpForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    username = StringField('username', validators=[DataRequired(), username_exists])
+    email = StringField('email', validators=[DataRequired(), email_exists])
     password = StringField('password', validators=[DataRequired()])
