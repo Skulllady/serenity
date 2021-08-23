d60bbeb66760314e0606d06f9434dfa94ac0f5d0
Skulllady <noorhoque45@gmail.com>
2021-08-22 23:47:18 -0400

    working on adding account holder's name at top

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../store/session";
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
+  const account = useSelector(state => {
+    return state.account
+  })
   const onLogout = async (e) => {
     dispatch(logout());
   };
 
-  return <button onClick={onLogout}> 🔓︎Logout</button>;
-};
+  return (
+    <div id="logged-in-navbar">
+      <button onClick={onLogout}> 🔓︎Logout</button>
+      {/* {account.list.map((account) => {
+        return (
+          <div className="account" key={account.id}>
+            <h2>Hello, {account.name}</h2>
+          </div>
+        )
+      })
+      }; */}
+    </div>
+  );
+}
 
 export default LogoutButton;
