da3ef70b260a47cd15046c5f0a11e5a594e7647c
Skulllady <noorhoque45@gmail.com>
2021-10-07 15:02:52 -0400

    added account name to welcome message

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { displayAccounts } from "../store/account.js";
 import LogoutButton from './auth/LogoutButton';
 import logo from '../images/logo/logo_transparent.png';
 import '../components/stylesheets/nav.css';
 import "../components/stylesheets/index.css";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const account = useSelector(state => {
+    return state.account
+  })
+
+  useEffect(() => {
+    dispatch(displayAccounts());
+  }, [dispatch]);
+
   return (
     <nav className="auth-nav">
       <div className="nav-items">
@@ -14,6 +25,16 @@ const NavBar = () => {
         </NavLink>
       </div>
       <div className="nav-items">
+        {account.list.map((account) => {
+          console.log("ACCOUNNNNNT", account)
+          if (true) {
+            let account_name = account.account_name;
+            return (
+              <div key={account_name}>Welcome to Serenity, {account_name}</div>
+            )
+          }
+        })}
+
         <LogoutButton />
       </div>
     </nav>
